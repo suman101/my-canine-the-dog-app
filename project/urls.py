afaff7bf97cf8bad1c562dd1c00b3b88a90b072f
suman101 <sumanlamichhane45@gmail.com>
2022-03-12 12:28:37 +0545

    push

--- project/urls.py
+++ project/urls.py
@@ -19,13 +19,31 @@ from drf_spectacular.views import SpectacularAPIView,SpectacularSwaggerView
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API Doc",
+      default_version='v1',
+      description="School Managemet System",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@abc.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
     path('dogapp/', include('dogapp.urls')),
     path('accounts/', include('allauth.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(),name='schema'),
-    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
