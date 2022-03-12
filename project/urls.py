295922e7bf54c00cac90d09861d41dd9c10255a2
suman101 <sumanlamichhane45@gmail.com>
2022-03-12 12:25:28 +0545

    push

--- project/urls.py
+++ project/urls.py
@@ -28,9 +28,8 @@ urlpatterns = [
     path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
-if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
         
 
 
