869ae217f35e250aa646515d17d674f92483a2b5
suman101 <sumanlamichhane45@gmail.com>
2022-03-14 18:00:53 +0545

    app.py

--- authentication/models.py
+++ authentication/models.py
@@ -51,3 +51,5 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
         # to:
         [reset_password_token.user.email]
     )
+
+
--- authentication/signals.py
+++ authentication/signals.py
@@ -1,15 +1,17 @@
 from django.db.models.signals import post_save
 from django.contrib.auth import get_user_model
 from django.dispatch import receiver
-from .models import Profile
+from django.contrib.auth import user_logged_in,user_logged_out
+from .models import UserProfile
+
 
 User = get_user_model()
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        UserProfile.objects.create(user=instance)
 
-@receiver(post_save,sender=User)
-def saveProfile(sender,instance,**kwargs):
-    instance.profile.save()
+
+# TO CHECK ONLINE AND OFF-LINE STATUS OF USERS
+
