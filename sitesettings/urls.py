28e50c81a64bcff9855c6bcce6157e9d294d5a82
suman101 <sumanlamichhane45@gmail.com>
2022-03-16 17:03:53 +0545

    sitesettings urls

--- sitesettings/urls.py
+++ sitesettings/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import SmtpView, SmtpDetailView
+from .views import SmtpView, SmtpDetailView, ContactUsListView, ContactUsCreateView, ContactUsDetailView, ContactUsUpdateView, ContactUsDeleteView
 
 urlpatterns = [
     path('smtp/',SmtpView.as_view()),
     path('smtp-detail/',SmtpDetailView.as_view()),
+
+    path('contact-us-list/', ContactUsListView.as_view()),
+    path('contact-us-create/', ContactUsCreateView.as_view()),
+    path('contact-us-detail/<int:pk>/', ContactUsDetailView.as_view()),
+    path('contact-us-update/<int:pk>/', ContactUsUpdateView.as_view()),
+    path('contact-us-delete/<int:pk>/', ContactUsDeleteView.as_view()),
 ]
