95addba297a48054c67ca8d98365e2205dc96f10
suman101 <sumanlamichhane45@gmail.com>
2022-03-17 12:39:44 +0545

    category

--- dogapp/admin.py
+++ dogapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post,Comment,Like,Message,PetProfile,Breed,Training
+from .models import Post,Comment,Like,Message,PetProfile,Breed,Training,Category
 # Register your models here.
 
 admin.site.register(Post)
@@ -9,3 +9,4 @@ admin.site.register(Message)
 admin.site.register(PetProfile)
 admin.site.register(Breed)
 admin.site.register(Training)
+admin.site.register(Category)
