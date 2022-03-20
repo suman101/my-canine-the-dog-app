55c9f144e0699b036b0019156e2ff43add16e436
suman101 <sumanlamichhane45@gmail.com>
2022-03-20 15:09:42 +0545

    breed

--- dogapp/models.py
+++ dogapp/models.py
@@ -47,7 +47,7 @@ class Message(models.Model):
 class Breed(models.Model):
     title = models.CharField(max_length=55)
     description = models.TextField()
-    image = models.ImageField(upload_to = 'images/')
+    image = models.ImageField(upload_to = 'images/',null=True,blank=True)
     
     def __str__(self):
         return self.title
