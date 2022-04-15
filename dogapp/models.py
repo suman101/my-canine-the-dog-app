d59ff87639f2f91c73acec7bf611cc91a97a5953
suman101 <sumanlamichhane45@gmail.com>
2022-04-15 22:41:40 +0545

    push

--- dogapp/models.py
+++ dogapp/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
         ordering = ('created',)
 
     def __str__(self): 
-        return self.user
+        return self.pet_name
     
 class Comment(models.Model):
     comment = models.TextField(max_length=254, blank=True, null=True)
